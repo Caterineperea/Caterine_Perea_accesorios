0fa4816b94a46b66256f814e7d397a1947f9fcb0
Caterineperea <caterineperea1124101@correo.itm.edu.co>
2025-08-28 07:08:45 -0500

    SubirRepositorio

--- Caterine_Perea_accesorios/Program.cs
+++ Caterine_Perea_accesorios/Program.cs
@@ -1,2 +1,53 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var accesorio = new Accesorios(); 
+accesorio.Id = 1;
+accesorio.Nombre = "Manilla acero";
+accesorio.tipo = new Tipos() { Id = 1, Nombre = "Manilla" };
+accesorio.Valor = 12000.0m;
+accesorio.FechaCompra = DateTime.Now;
+accesorio.Existe = true;
+accesorio.Materiales = new List<Materiales>();
+accesorio.Materiales.Add(new Materiales() { Id = 1, Nombrem = "Fimo" });
+accesorio.Materiales.Add(new Materiales() { Id = 2, Nombrem = "Perla" });
+
+accesorio.Id = 2;
+accesorio.Nombre = "Aretas";
+accesorio.tipo = new Tipos() { Id = 1, Nombre = "Tejidos" };
+accesorio.Valor = 25000.0m;
+accesorio.FechaCompra = DateTime.Now;
+accesorio.Existe = true;
+accesorio.Materiales = new List<Materiales>();
+accesorio.Materiales.Add(new Materiales() { Id = 1, Nombrem = "Mostacilla" });
+accesorio.Materiales.Add(new Materiales() { Id = 2, Nombrem = "Base acero" });
+
+public class Tipos
+{
+    public int Id = 0;
+    public String? Nombre = "";
+    public List<Accesorios> Accesorios = new List<Accesorios>(); //plural lista de arreglos
+}
+
+public class Materiales
+{
+    public int Id = 0;
+    public String? Nombrem;
+}
+
+public class Calidad
+{
+    public int Id = 0;
+    public bool Antialergico = true;
+}
+
+public class Accesorios
+{
+    public int Id = 0;
+    public String? Nombre = "";
+    public decimal Valor = 0.0m;
+    public DateTime FechaCompra = DateTime.Now;
+    public bool Existe = true;
+    public Tipos tipo = new Tipos(); //singular un solo dato
+    public List<Materiales> Materiales = new List<Materiales>(); //plural lista de arreglos
+}
