c1bd66a7379ff94bbb1a275013b78fb5d40d090d
Caterineperea <caterineperea1124101@correo.itm.edu.co>
2025-08-28 07:49:56 -0500

    ConflictoWeb2.cs

--- Caterine_Perea_accesorios/Program.cs
+++ Caterine_Perea_accesorios/Program.cs
@@ -11,6 +11,7 @@ accesorio.Existe = true;
 accesorio.Materiales = new List<Materiales>();
 accesorio.Materiales.Add(new Materiales() { Id = 1, Nombrem = "Fimo" });
 accesorio.Materiales.Add(new Materiales() { Id = 2, Nombrem = "Perla" });
+accesorio.Materiales.Add(new Materiales() { Id = 3, Nombrem = "Plastimetal" });
 accesorio.calidad = new Calidades() { Id = 1, Antialergico = false };
 
 
